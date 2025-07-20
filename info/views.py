49b3f21dadd25f65077d58476201ef59a1530889
MacarenaQuijadaG <mcarena.quijada@outlook.com>
2025-07-20 00:22:51 -0400

    Se incorpora mas templates como footer y navbar

--- info/views.py
+++ info/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-<<<<<<< HEAD
-    return render(request, 'info/base.html')
-=======
-    return render(request, 'info/index.html')  
 
->>>>>>> 4389f3ad855d186a2ed5c6c16f87cac0b7865045
+    return render(request, 'info/base.html')
+
+    return render(request, 'info/index.html')  
