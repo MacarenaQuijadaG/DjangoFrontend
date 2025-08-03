6f5fb8908f778e7029494db6c91c6d0a29839305
MacarenaQuijadaG <mcarena.quijada@outlook.com>
2025-08-03 00:19:02 -0400

    Se respalda codigo completo

--- info/views.py
+++ info/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 
-    return render(request, 'info/base.html')
+   # return render(request, 'info/base.html')
     return render(request, 'info/index.html')  
